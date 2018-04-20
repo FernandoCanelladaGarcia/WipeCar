5d5d368e9e1c8d77caf71851c04b12de89ff846f
Fernando Canellada <fercanellada@gmail.com>
2018-04-20 15:47:59 +0100

    Update AppMediador

--- app/src/main/java/tfg/android/fcg/AppMediador.java
+++ app/src/main/java/tfg/android/fcg/AppMediador.java
@@ -8,24 +8,68 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 
+import tfg.android.fcg.presentador.IPresentadorHistorial;
+import tfg.android.fcg.presentador.IPresentadorLogin;
+import tfg.android.fcg.presentador.IPresentadorMapaOrigen;
+import tfg.android.fcg.presentador.IPresentadorOTGConductor;
+import tfg.android.fcg.presentador.IPresentadorOTGPasajero;
+import tfg.android.fcg.presentador.IPresentadorPerfil;
+import tfg.android.fcg.presentador.IPresentadorPrincipal;
+import tfg.android.fcg.presentador.IPresentadorRegistro;
+import tfg.android.fcg.presentador.IPresentadorVehiculo;
+import tfg.android.fcg.presentador.PresentadorHistorial;
+import tfg.android.fcg.presentador.PresentadorLogin;
+import tfg.android.fcg.presentador.PresentadorMapaOrigen;
+import tfg.android.fcg.presentador.PresentadorOTGConductor;
+import tfg.android.fcg.presentador.PresentadorOTGPasajero;
+import tfg.android.fcg.presentador.PresentadorPerfil;
+import tfg.android.fcg.presentador.PresentadorPrincipal;
+import tfg.android.fcg.presentador.PresentadorRegistro;
+import tfg.android.fcg.presentador.PresentadorVehiculo;
+import tfg.android.fcg.vista.IVistaHistorial;
+import tfg.android.fcg.vista.IVistaLogin;
+import tfg.android.fcg.vista.IVistaMapaOrigen;
+import tfg.android.fcg.vista.IVistaOTGConductor;
+import tfg.android.fcg.vista.IVistaOTGPasajero;
+import tfg.android.fcg.vista.IVistaPerfil;
+import tfg.android.fcg.vista.IVistaPrincipal;
+import tfg.android.fcg.vista.IVistaRegistro;
+import tfg.android.fcg.vista.IVistaVehiculo;
+
 
 @SuppressWarnings("rawtypes")
 public class AppMediador extends Application {
 
 	private static AppMediador singleton;
 
-	// presenters and views members
-//	private IEMERAppPresenter emmerAppPresenter;
-//	private IEMERAppView emmerAppView;
-//	private IInsertPresenter insertPresenter;
-//	private IInsertView insertView;
-//	private IDetailPresenter detailPresenter;
-//	private IDetailView detailView;
-//	private IMapPresenter mapPresenter;
+	//variables correspondientes a los presentadores, vistas y modelo.
 
-	public static AppMediador getInstance(){
-		return singleton;
-	}
+	private IVistaLogin vistaLogin;
+	private IPresentadorLogin presentadorLogin;
+
+	private IVistaRegistro vistaRegistro;
+	private IPresentadorRegistro presentadorRegistro;
+
+	private IVistaMapaOrigen vistaMapaOrigen;
+	private IPresentadorMapaOrigen presentadorMapaOrigen;
+
+	private IVistaPrincipal vistaPrincipal;
+	private IPresentadorPrincipal presentadorPrincipal;
+
+	private IVistaOTGPasajero vistaOTGPasajero;
+	private IPresentadorOTGPasajero presentadorOTGPasajero;
+
+	private IVistaOTGConductor vistaOTGConductor;
+	private IPresentadorOTGConductor presentadorOTGConductor;
+
+	private IVistaPerfil vistaPerfil;
+	private IPresentadorPerfil presentadorPerfil;
+
+	private IVistaVehiculo vistaVehiculo;
+	private IPresentadorVehiculo presentadorVehiculo;
+
+	private IVistaHistorial vistaHistorial;
+	private IPresentadorHistorial presentadorHistorial;
 
 	// constants of commuinication, storage and petition
 	public static final String NOTIFICATION_OPERATIONS_RECOVERED = "android.cnog.NOTIFICATION_OPERATIONS_RECOVERED";
@@ -58,80 +102,180 @@ public class AppMediador extends Application {
 
     public static boolean permission_granted = true;
 
-	// Accessor methods to presenters and views
-//	public IEMERAppPresenter getEmmerAppPresenter() {
-//		if (emmerAppPresenter == null)
-//			emmerAppPresenter = new EMERAppPresenter();
-//		return emmerAppPresenter;
-//	}
-//
-//	public void removeEmmerAppPresenter() {
-//		emmerAppPresenter = null;
-//	}
-//
-//    public IInsertPresenter getInsertPresenter() {
-//        if (insertPresenter == null)
-//            insertPresenter = new InsertPresenter();
-//        return insertPresenter;
-//    }
-//
-//    public void removeInsertPresenter() {
-//        insertPresenter = null;
-//    }
-//
-//    public IDetailPresenter getDetailPresenter() {
-//        if (detailPresenter == null)
-//            detailPresenter = new DetailPresenter();
-//        return detailPresenter;
-//    }
-//
-//    public void removeDetailPresenter() {
-//        detailPresenter = null;
-//    }
-//
-//	public IMapPresenter getMapPresenter() {
-//		if (mapPresenter == null)
-//			mapPresenter = new MapPresenter();
-//		return mapPresenter;
-//	}
-//
-//	public void removeMapPresenter() {
-//		mapPresenter = null;
-//	}
-//
-//	public IEMERAppView getEmmerAppView() {
-//		return emmerAppView;
-//	}
-//
-//	public void setEmmerAppView(IEMERAppView emmerAppView) {
-//
-//		this.emmerAppView = emmerAppView;
-//	}
-//
-//	public IInsertView getInsertView() {
-//	    return insertView;
-//    }
-//
-//    public void setInsertView(IInsertView insertView) {
-//	    this.insertView = insertView;
-//    }
-//
-//    public IDetailView getDetailView() {
-//	    return detailView;
-//    }
-//
-//    public void setDetailView(IDetailView detailView) {
-//	    this.detailView = detailView;
-//    }
-//
-//	// Navigation and service definition methods
-//    public Class navegateToInsertView() {
-//	    return InsertView.class;
-//    }
-//
-//    public Class navegateToDetailView() { return DetailView.class; }
+	public static AppMediador getInstance(){
+		return singleton;
+	}
 
-	// Methods to manage Android components
+	//Métodos accesor de los presentadores, vistas y modelo.
+
+
+	//PRESENTADOR
+	
+	public IPresentadorLogin getPresentadorLogin() {
+		if(presentadorLogin == null)
+			presentadorLogin = new PresentadorLogin();
+		return presentadorLogin;
+	}
+
+	public void removePresentadorLogin(){
+		presentadorLogin = null;
+	}
+
+	public IPresentadorRegistro getPresentadorRegistro() {
+		if(presentadorRegistro == null)
+			presentadorRegistro = new PresentadorRegistro();
+		return presentadorRegistro;
+	}
+
+	public void removePresentadorRegistro(){
+		presentadorRegistro = null;
+	}
+
+	public IPresentadorMapaOrigen getPresentadorMapaOrigen() {
+		if(presentadorMapaOrigen == null)
+			presentadorMapaOrigen = new PresentadorMapaOrigen();
+		return presentadorMapaOrigen;
+	}
+
+	public void removePresentadorMapaOrigen(){
+		presentadorMapaOrigen = null;
+	}
+
+	public IPresentadorPrincipal getPresentadorPrincipal() {
+		if(presentadorPrincipal == null)
+			presentadorPrincipal = new PresentadorPrincipal();
+		return presentadorPrincipal;
+	}
+
+	public void removePresentadorPrincipal(){
+		presentadorPrincipal = null;
+	}
+
+	public IPresentadorOTGPasajero getPresentadorOTGPasajero() {
+		if(presentadorOTGPasajero == null)
+			presentadorOTGPasajero = new PresentadorOTGPasajero();
+		return presentadorOTGPasajero;
+	}
+
+	public void removePresentadorOTGPasajero(){
+		presentadorOTGPasajero = null;
+	}
+
+	public IPresentadorOTGConductor getPresentadorOTGConductor() {
+		if(presentadorOTGConductor == null)
+			presentadorOTGConductor = new PresentadorOTGConductor();
+		return presentadorOTGConductor;
+	}
+
+	public void removePresentadorOTGConductor(){
+		presentadorOTGConductor = null;
+	}
+
+	public IPresentadorPerfil getPresentadorPerfil() {
+		if(presentadorPerfil == null)
+			presentadorPerfil = new PresentadorPerfil();
+		return presentadorPerfil;
+	}
+
+	public void removePresentadorPerfil(){
+		presentadorPerfil = null;
+	}
+
+	public IPresentadorVehiculo getPresentadorVehiculo() {
+		if(presentadorVehiculo == null)
+			presentadorVehiculo = new PresentadorVehiculo();
+		return presentadorVehiculo;
+	}
+
+	public void removePresentadorVehiculo(){
+		presentadorVehiculo = null;
+	}
+
+	public IPresentadorHistorial getPresentadorHistorial() {
+		if(presentadorHistorial == null)
+			presentadorHistorial = new PresentadorHistorial();
+		return presentadorHistorial;
+	}
+
+	public void removePresentadorHistorial(){
+		presentadorHistorial = null;
+	}
+
+	//VISTA
+
+	public IVistaLogin getVistaLogin() {
+		return vistaLogin;
+	}
+
+	public void setVistaLogin(IVistaLogin vistaLogin){
+		this.vistaLogin = vistaLogin;
+	}
+
+	public IVistaRegistro getVistaRegistro() {
+		return vistaRegistro;
+	}
+
+	public void setVistaRegistro(IVistaRegistro vistaRegistro){
+		this.vistaRegistro = vistaRegistro;
+	}
+
+	public IVistaMapaOrigen getVistaMapaOrigen() {
+		return vistaMapaOrigen;
+	}
+
+	public void setVistaMapaOrigen(IVistaMapaOrigen vistaMapaOrigen){
+		this.vistaMapaOrigen = vistaMapaOrigen;
+	}
+
+	public IVistaPrincipal getVistaPrincipal() {
+		return vistaPrincipal;
+	}
+
+	public void setVistaPrincipal(IVistaPrincipal vistaPrincipal){
+		this.vistaPrincipal = vistaPrincipal;
+	}
+
+	public IVistaOTGPasajero getVistaOTGPasajero() {
+		return vistaOTGPasajero;
+	}
+
+	public void setVistaOTGPasajero(IVistaOTGPasajero vistaOTGPasajero){
+		this.vistaOTGPasajero = vistaOTGPasajero;
+	}
+
+	public IVistaOTGConductor getVistaOTGConductor() {
+		return vistaOTGConductor;
+	}
+
+	public void setVistaOTGConductor(IVistaOTGConductor vistaOTGConductor){
+		this.vistaOTGConductor = vistaOTGConductor;
+	}
+
+	public IVistaPerfil getVistaPerfil() {
+		return vistaPerfil;
+	}
+
+	public void setVistaPerfil(IVistaPerfil vistaPerfil){
+		this.vistaPerfil = vistaPerfil;
+	}
+
+	public IVistaVehiculo getVistaVehiculo() {
+		return vistaVehiculo;
+	}
+
+	public void setVistaVehiculo(IVistaVehiculo vistaVehiculo){
+		this.vistaVehiculo = vistaVehiculo;
+	}
+
+	public IVistaHistorial getVistaHistorial() {
+		return vistaHistorial;
+	}
+
+	public void setVistaHistorial(IVistaHistorial vistaHistorial){
+		this.vistaHistorial = vistaHistorial;
+	}
+
+	// Metodos que manejan componentes de Android
 
 	public void launchActivity(Class toActivity, Object fromComponent, Bundle extras) {
 		Intent i = new Intent(this, toActivity);
@@ -185,9 +329,6 @@ public class AppMediador extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-//		emmerAppPresenter = null;
-//        insertPresenter = null;
-//        detailPresenter = null;
 		singleton = this;
 	}
 }
